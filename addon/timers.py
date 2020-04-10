8ffe8d512b70c6512c1d25564ade6193998501e2
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-10 20:52:40 +0200

    Make autosave_timer actually save
    If there is a file to save to, and there's new data to save, go ahead and save.
    Also register now doesn't call unregister anymore, but checks whether it's already registered instead. More on this later.

--- addon/timers.py
+++ addon/timers.py
@@ -5,16 +5,16 @@ from . import utils
 def autosave_timer():
     prefs = utils.get_prefs()
 
-    print("AUTO SAVING")
+    if bpy.data.is_saved and bpy.data.is_dirty:
+        bpy.ops.wm.save_mainfile(check_existing=False)
 
     return prefs.autosave_interval * 60
 
 
 def register():
-    unregister()
-
-    interval = utils.get_prefs().autosave_interval * 60
-    bpy.app.timers.register(autosave_timer, first_interval=interval, persistent=True)
+    if not bpy.app.timers.is_registered(autosave_timer):
+        interval = utils.get_prefs().autosave_interval * 60
+        bpy.app.timers.register(autosave_timer, first_interval=interval, persistent=True)
 
 
 def unregister():
