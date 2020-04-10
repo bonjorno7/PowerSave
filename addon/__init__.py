ab244942e5631edeea1a84f8d210076cb421afdb
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-10 16:58:06 +0200

    Rename timer.py to timers.py
    For consistency and because who knows, maybe I'll need another timer at some point.

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,14 +1,14 @@
 import bpy
 from . import utils
 from . import prefs
-from . import timer
+from . import timers
 
 
 def register():
     bpy.utils.register_class(prefs.PowerSavePrefs)
-    bpy.app.timers.register(timer.autosave_timer, persistent=True)
+    bpy.app.timers.register(timers.autosave_timer, persistent=True)
 
 
 def unregister():
-    bpy.app.timers.unregister(timer.autosave_timer)
+    bpy.app.timers.unregister(timers.autosave_timer)
     bpy.utils.unregister_class(prefs.PowerSavePrefs)
