78184bffaf7d431230ad72f3c7dc991d5ec3dff2
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-12 15:09:20 +0200

    Add descriptions to all preferences
    These show up when you hover over them. Also made a utility function that lets you make a multiline description correctly, I think it's nice.

--- addon/prefs/prefs.py
+++ addon/prefs/prefs.py
@@ -7,12 +7,14 @@ class PowerSavePrefs(bpy.types.AddonPreferences):
 
     base_folder: bpy.props.StringProperty(
         name="Base Folder",
+        description="The directory where initial saves will be stored",
         default=common.get_default_folder(),
         subtype='FILE_PATH',
     )
 
     autosave_interval: bpy.props.IntProperty(
         name="Autosave Interval",
+        description="The amount of minutes between each autosave",
         default=2,
         min=1,
         max=60,
@@ -20,11 +22,17 @@ class PowerSavePrefs(bpy.types.AddonPreferences):
 
     save_on_startup: bpy.props.BoolProperty(
         name="Save on Startup",
+        description="Whether to save new projects in your base folder automatically",
         default=True,
     )
 
     datetime_format: bpy.props.StringProperty(
         name="Datetime Format",
+        description=common.description(
+            "The formatting string used to create file names",
+            "%Y = year    %m = month    %d = day",
+            "%H = hour    %M = minute    %S = second",
+        ),
         default="%Y-%m-%d__%H-%M-%S__01.blend",
     )
 
--- addon/utils/common.py
+++ addon/utils/common.py
@@ -11,6 +11,10 @@ def get_prefs():
     return bpy.context.preferences.addons[module_name].preferences
 
 
+def description(*args):
+    return ".\n".join(args)
+
+
 def get_default_folder():
     temporary_directory = bpy.context.preferences.filepaths.temporary_directory
     return str(pathlib.Path(temporary_directory).joinpath("PowerSave"))
