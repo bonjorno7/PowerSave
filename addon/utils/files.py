7dd9f8d67bae7c8bef5579663a600efc1d53de85
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-13 23:23:21 +0200

    Add utils add_to_recent_files and open_project_folder
    Saving and loading from bpy does not add to recent files list, so we do it manually.
    Opening the project folder is useful all the time.

--- addon/utils/files.py
+++ addon/utils/files.py
@@ -1,7 +1,45 @@
 import bpy
 import pathlib
+import sys
+import subprocess
 
 
 def get_default_folder():
     temporary_directory = bpy.context.preferences.filepaths.temporary_directory
     return str(pathlib.Path(temporary_directory).joinpath("PowerSave"))
+
+
+def add_to_recent_files():
+    try:
+        with open(bpy.utils.user_resource('CONFIG', "recent-files.txt"), "r+") as recent_files:
+            lines = [bpy.data.filepath]
+
+            for line in recent_files:
+                line = line.rstrip("\r\n")
+
+                if line != bpy.data.filepath:
+                    lines.append(line)
+
+            content = "\n".join(lines)
+
+            recent_files.seek(0, 0)
+            recent_files.write(content)
+
+    except:
+        print("Failed to add to recent files")
+
+
+def open_project_folder():
+    path = str(pathlib.Path(bpy.data.filepath).parent)
+
+    if sys.platform == "win32":
+        subprocess.Popen(["explorer", path])
+
+    elif sys.platform == "linux":
+        subprocess.Popen(["xdg-open", path])
+
+    elif sys.platform == "darwin":
+        subprocess.Popen(["open", path])
+
+    else:
+        print("Failed to open project folder")
