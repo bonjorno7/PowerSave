c24994df52b17ebc00d60699447d6aee382c5968
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-14 20:32:53 +0200

    Create get increment and get filename utility functions
    This gets the increment format from prefs. If it doesn't contain any numbers, we will append one. Get filename just combines datetime and increment.

--- addon/utils/common.py
+++ addon/utils/common.py
@@ -1,5 +1,6 @@
 import bpy
 import datetime
+import re
 
 
 module_name = __name__.partition('.')[0]
@@ -16,3 +17,13 @@ def description(*args):
 def get_datetime():
     string = get_prefs().datetime_format
     return datetime.datetime.now().strftime(string)
+
+
+def get_increment():
+    string = get_prefs().increment_format
+    numbers = re.findall(r"\d+", string)
+    return string if numbers else f"{string}1"
+
+
+def get_filename():
+    return f"{get_datetime()}{get_increment()}"
