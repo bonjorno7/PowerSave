4c86b55ada6c1574584580f2bdde6b28b18efbd0
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-14 13:22:48 +0200

    Make load handler use save datetime util
    That's nice isn't it

--- addon/handlers/load_handler.py
+++ addon/handlers/load_handler.py
@@ -1,21 +1,11 @@
 import bpy
-import pathlib
 from .. import utils
-from .. import timers
 
 
 @bpy.app.handlers.persistent
 def load_handler(dummy):
     prefs = utils.common.get_prefs()
 
-    if prefs.save_on_startup and prefs.base_folder and not bpy.data.is_saved:
-        name = f"{utils.common.get_datetime(prefs.datetime_format)}.blend"
-        path = pathlib.Path(prefs.base_folder).joinpath(name).resolve()
-
-        try:
-            path.parent.mkdir(parents=True, exist_ok=True)
-            bpy.ops.wm.save_mainfile(filepath=str(path))
-            utils.files.add_to_recent_files()
-
-        except:
-            print(f"Failed to save {path}")
+    if prefs.save_on_startup:
+        result = utils.save.save_datetime()
+        print(result[1])
