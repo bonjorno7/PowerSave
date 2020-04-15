32e74c932673b981c555cd12089131f9b2249061
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-15 22:38:22 +0200

    Register powersave operator
    Felt like giving this its own commit

--- addon/ops/__init__.py
+++ addon/ops/__init__.py
@@ -1,10 +1,12 @@
 import bpy
+from . import powersave
 from . import save_datetime
 from . import save_incremental
 from . import open_project_folder
 
 
 def register():
+    bpy.utils.register_class(powersave.PowerSave)
     bpy.utils.register_class(save_datetime.SaveDatetime)
     bpy.utils.register_class(save_incremental.SaveIncremental)
     bpy.utils.register_class(open_project_folder.OpenProjectFolder)
@@ -14,3 +16,4 @@ def unregister():
     bpy.utils.unregister_class(open_project_folder.OpenProjectFolder)
     bpy.utils.unregister_class(save_incremental.SaveIncremental)
     bpy.utils.unregister_class(save_datetime.SaveDatetime)
+    bpy.utils.unregister_class(powersave.PowerSave)
