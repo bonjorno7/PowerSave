79567ecf77493458155cac53d754a11a2cad9fa2
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-16 12:49:20 +0200

    Remove save datetime and save incremental operators
    PowerSave does what they do now so, yeet

--- addon/ops/__init__.py
+++ addon/ops/__init__.py
@@ -1,19 +1,13 @@
 import bpy
 from . import powersave
-from . import save_datetime
-from . import save_incremental
 from . import open_project_folder
 
 
 def register():
     bpy.utils.register_class(powersave.PowerSave)
-    bpy.utils.register_class(save_datetime.SaveDatetime)
-    bpy.utils.register_class(save_incremental.SaveIncremental)
     bpy.utils.register_class(open_project_folder.OpenProjectFolder)
 
 
 def unregister():
     bpy.utils.unregister_class(open_project_folder.OpenProjectFolder)
-    bpy.utils.unregister_class(save_incremental.SaveIncremental)
-    bpy.utils.unregister_class(save_datetime.SaveDatetime)
     bpy.utils.unregister_class(powersave.PowerSave)
