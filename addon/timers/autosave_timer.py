701a035880d3bdf2512acf0047b9008b1bea8427
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-26 15:24:36 +0200

    Use save copy for autosave
    This works perfectly but it makes the current blend think that it's saved too, which is not true. That means green icon too, and no prompt when you quit, somewhat misleading. Another problem is that the .autosave file is overwritten from the main file every autosave, which when you load the main file again, you'll lose your work from the .autosave when the autosave timer happens.

--- addon/timers/autosave_timer.py
+++ addon/timers/autosave_timer.py
@@ -1,4 +1,5 @@
 import bpy
+import pathlib
 from .. import utils
 
 
@@ -6,6 +7,11 @@ def autosave_timer():
     prefs = utils.common.prefs()
 
     if prefs.use_autosave and bpy.data.is_saved and bpy.data.is_dirty:
-        bpy.ops.wm.save_mainfile(check_existing=False)
+        path = pathlib.Path(bpy.data.filepath)
+
+        if prefs.autosave_to_copy:
+            path = path.with_suffix('.autosave')
+
+        bpy.ops.wm.save_as_mainfile(filepath=str(path), check_existing=False, copy=True)
 
     return prefs.autosave_interval * 60
