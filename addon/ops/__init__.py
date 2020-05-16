24f792c2b1e3d930131d27c8f3ee77a9a6abc10c
bonjorno7 <jorijndegraaf@gmail.com>
2020-05-16 21:50:59 +0200

    Register LoadAutosave operator
    It's internal btw cause it's only meant to be called from python, after checking should load autosave

--- addon/ops/__init__.py
+++ addon/ops/__init__.py
@@ -1,13 +1,16 @@
 import bpy
 from . import powersave
+from . import load_autosave
 from . import open_project_folder
 
 
 def register():
     bpy.utils.register_class(powersave.PowerSave)
+    bpy.utils.register_class(load_autosave.LoadAutosave)
     bpy.utils.register_class(open_project_folder.OpenProjectFolder)
 
 
 def unregister():
     bpy.utils.unregister_class(open_project_folder.OpenProjectFolder)
+    bpy.utils.unregister_class(load_autosave.LoadAutosave)
     bpy.utils.unregister_class(powersave.PowerSave)
