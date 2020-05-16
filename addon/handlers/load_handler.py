557c6ea193684dd189631a0a9bca319dddf441f1
bonjorno7 <jorijndegraaf@gmail.com>
2020-05-16 21:53:41 +0200

    Update load handler to use load autosave
    Also made it use the powersave operator instead of the utility function so that the console shows more consistent information. Changes nothing for the end user.

--- addon/handlers/load_handler.py
+++ addon/handlers/load_handler.py
@@ -4,10 +4,12 @@ from .. import utils
 
 @bpy.app.handlers.persistent
 def load_handler(_):
+    utils.common.update_powersave_name()
     prefs = utils.common.prefs()
 
-    utils.common.update_powersave_name()
-
     if prefs.save_on_startup and not bpy.data.is_saved:
-        result = utils.save.powersave()
-        print(result[1])
+        bpy.ops.powersave.powersave()
+
+    if prefs.autosave_to_copy and bpy.data.is_saved:
+        if utils.save.should_load_autosave():
+            bpy.ops.powersave.load_autosave('INVOKE_DEFAULT')
