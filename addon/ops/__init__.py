b36a62491edf35ea8adc00bb2f38e4f87a8895de
bonjorno7 <jorijndegraaf@gmail.com>
2020-05-17 18:07:49 +0200

    Add LoadPrevious and LoadNext operators and register them
    Perhaps checking whether the file exists in poll is overkill, but I do like the button being grayed out when you're not supposed to click it, so idk.

--- addon/ops/__init__.py
+++ addon/ops/__init__.py
@@ -1,16 +1,22 @@
 import bpy
 from . import powersave
 from . import load_autosave
+from . import load_previous
+from . import load_next
 from . import open_project_folder
 
 
 def register():
     bpy.utils.register_class(powersave.PowerSave)
     bpy.utils.register_class(load_autosave.LoadAutosave)
+    bpy.utils.register_class(load_previous.LoadPrevious)
+    bpy.utils.register_class(load_next.LoadNext)
     bpy.utils.register_class(open_project_folder.OpenProjectFolder)
 
 
 def unregister():
     bpy.utils.unregister_class(open_project_folder.OpenProjectFolder)
+    bpy.utils.unregister_class(load_next.LoadNext)
+    bpy.utils.unregister_class(load_previous.LoadPrevious)
     bpy.utils.unregister_class(load_autosave.LoadAutosave)
     bpy.utils.unregister_class(powersave.PowerSave)
