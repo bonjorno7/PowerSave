83ef75371f6e57ac3ebba0362a8fe0b6c3488faf
bonjorno7 <jorijndegraaf@gmail.com>
2021-09-17 16:47:09 +0200

    Delay autosave if BoxCutter is running
    I was supposed to implement this months ago but forgot.
    Anyway, if BoxCutter is currently cutting, it is not wise to save, as doing so can cause unexpected things to happen.
    Because a cut takes a very short amount of time, I just tell the autosave timer to try again in 1 second.

--- addon/timers/autosave_timer.py
+++ addon/timers/autosave_timer.py
@@ -6,6 +6,10 @@ def autosave_timer() -> float:
     prefs = utils.common.prefs()
 
     if prefs.use_autosave and bpy.data.is_saved and bpy.data.is_dirty:
+        scene = bpy.context.scene
+        if 'bc' in scene and scene.bc.running:
+            return 1
+
         current_path = utils.files.as_path(bpy.data.filepath)
         autosave_path = utils.files.as_autosave(current_path, mkdir=True)
         bpy.ops.wm.save_as_mainfile(filepath=str(autosave_path), check_existing=False, copy=True)
